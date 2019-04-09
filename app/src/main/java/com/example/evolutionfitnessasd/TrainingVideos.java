68e33bd051badf66f02e4790ccdf292ca4a8acc7
Franco <fra.pinto1994@gmail.com>
2019-04-09 17:07:53 +0200

    "Youtube api in recycler view added - TrainingVideos"

--- app/src/main/java/com/example/evolutionfitnessasd/TrainingVideos.java
+++ app/src/main/java/com/example/evolutionfitnessasd/TrainingVideos.java
@@ -1,30 +1,90 @@
 package com.example.evolutionfitnessasd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class TrainingVideos extends AppCompatActivity {
-    RecyclerView recyclerView;
-    Vector<YouTubeVideos> youTubeVideos = new Vector<>();
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training_videos);
-        recyclerView= (RecyclerView) findViewById(R.id.recyclerView);
+        setUpRecyclerView();
+        populateRecyclerView();
+
+    }
+
+    /**
+     * setup the recyclerview here
+     */
+    private void setUpRecyclerView() {
+        recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
 
-        youTubeVideos.add(new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"http://www.youtube.com/embed/bSMZknDI6bg\" frameborder=\"0\" allowfullscreen ng-show=\"showvideo\"></iframe>"));
-        youTubeVideos.add(new YouTubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"http://www.youtube.com/embed/ifXalt3MJtM\" frameborder=\"0\" allowfullscreen ng-show=\"showvideo\"></iframe>"));
-        VideoAdapter videoAdapter = new VideoAdapter(youTubeVideos);
-        recyclerView.setAdapter(videoAdapter);
+    /**
+     * populate the recyclerview and implement the click event here
+     */
+    private void populateRecyclerView() {
+        final ArrayList<YoutubeVideoModel> youtubeVideoModelArrayList = generateDummyVideoList();
+        YoutubeVideoAdapter adapter = new YoutubeVideoAdapter(this, youtubeVideoModelArrayList);
+        recyclerView.setAdapter(adapter);
+
+        //set click event
+        recyclerView.addOnItemTouchListener(new RecyclerViewOnClickListener(this, new RecyclerViewOnClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+
+                //start youtube player activity by passing selected video id via intent
+                startActivity(new Intent(TrainingVideos.this, YoutubePlayerActivity.class)
+                        .putExtra("video_id", youtubeVideoModelArrayList.get(position).getVideoId()));
+
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
+    }
 
 
+    /**
+     * method to generate dummy array list of videos
+     *
+     * @return
+     */
+    private ArrayList<YoutubeVideoModel> generateDummyVideoList() {
+        ArrayList<YoutubeVideoModel> youtubeVideoModelArrayList = new ArrayList<>();
 
+        //get the video id array, title array and duration array from strings.xml
+        String[] videoIDArray = getResources().getStringArray(R.array.video_id_array);
+        String[] videoTitleArray = getResources().getStringArray(R.array.video_title_array);
+        String[] videoDurationArray = getResources().getStringArray(R.array.video_duration_array);
+
+        //loop through all items and add them to arraylist
+        for (int i = 0; i < videoIDArray.length; i++) {
+
+            YoutubeVideoModel youtubeVideoModel = new YoutubeVideoModel();
+            youtubeVideoModel.setVideoId(videoIDArray[i]);
+            youtubeVideoModel.setTitle(videoTitleArray[i]);
+            youtubeVideoModel.setDuration(videoDurationArray[i]);
+
+            youtubeVideoModelArrayList.add(youtubeVideoModel);
+
+        }
+
+        return youtubeVideoModelArrayList;
     }
 }
---
+++ app/src/main/java/com/example/evolutionfitnessasd/YoutubeViewHolder.java
@@ -0,0 +1,24 @@
+package com.example.evolutionfitnessasd;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.youtube.player.YouTubeThumbnailView;
+/**
+ * Created by sonu on 10/11/17.
+ */
+
+public class YoutubeViewHolder extends RecyclerView.ViewHolder {
+
+    public YouTubeThumbnailView videoThumbnailImageView;
+    public TextView videoTitle, videoDuration;
+
+    public YoutubeViewHolder(View itemView) {
+        super(itemView);
+        videoThumbnailImageView = itemView.findViewById(R.id.video_thumbnail_image_view);
+        videoTitle = itemView.findViewById(R.id.video_title_label);
+        videoDuration = itemView.findViewById(R.id.video_duration_label);
+    }
+}
